5080ed4aeaa15d7deff29540af0f856e69bf1f5e
bearddude1 <baapteshuva@gmail.com>
2022-09-20 19:39:43 +0300

    ANSWERS 6

--- SQL_exercise_06/6_questions.sql
+++ SQL_exercise_06/6_questions.sql
@@ -2,4 +2,10 @@
 -- 6.1 List all the scientists' names, their projects' names, 
     -- and the hours worked by that scientist on each project, 
     -- in alphabetical order of project name, then scientist name.
+	SELECT S.NAME, P.NAME AS PROJECT, P.HOURS FROM Scientists S
+	JOIN AssignedTo ON S.SSN = Scientist
+	JOIN Projects P ON P.CODE = PROJECT 
+	ORDER BY P.NAME ASC, S.NAME ASC;
+	
 -- 6.2 Select the project names which are not assigned yet
+SELECT NAME FROM PROJECTS WHERE Code NOT IN (SELECT PROJECT FROM AssignedTo);
